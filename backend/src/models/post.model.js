a34c3cbbfe7d6f50b7a8394f3f9510892b544cdb
chihabSD <adamchihab2012@hotmail.com>
2019-07-23 21:41:43 +0100

    Adding post model Schema

---
+++ backend/src/models/post.model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+//get Schema from mongoose
+const { Schema } = mongoose;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  //like
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        require: true
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model("Post", PostSchema);
